05cc06a49b468ca7cfaab406caf37fc3ea776efe
Caroline Thordenberg <caroline.thordenberg@gmail.com>
2016-05-12 15:48:23 +0200

    hackelihack

---
+++ kits.js
@@ -0,0 +1,143 @@
+var fireplace_sipper = {
+	name: 'Fireplace Sipper',
+	categories: [
+		[2,0,0,0],
+		[1,0,0,1],
+		[2,0,1,1],
+		[3,1,1,0],
+		[0,0,3,3]
+	],
+	values: [2]
+}
+
+var smoking_guns = {
+	name: 'Smoking Guns',
+	categories: [
+		[0,2,0,1],
+		[1,2,3,0],
+		[2,0,2,3],
+		[0,0,0,2],
+		[0,0,3,2]
+	],
+	values: [3]
+}
+
+var berry_bunch = {
+	name: 'Berry Bunch',
+	categories: [
+		[2,0,0,0],
+		[0,1,1,2],
+		[2,0,0,2],
+		[1,1,3,2],
+		[0,0,3,3]
+	],
+	values: [2]
+}
+
+var liquid_dessert = {
+	name: 'Liquid Dessert',
+	categories: [
+		[3,0,0,0],
+		[0,2,0,2],
+		[0,1,0,2],
+		[3,2,2,0],
+		[0,2,0,3]
+	],
+	values: [1]
+}
+
+var day_at_the_ocean = {
+	name: 'Day at the Ocean',
+	categories: [
+		[0,3,0,1],
+		[0,1,2,0],
+		[2,0,3,2],
+		[2,0,0,1],
+		[0,3,0,1]
+	],
+	values: [3]
+}
+
+var wild_wild_west = {
+	name: 'Wild Wild West',
+	categories: [
+		[2,0,0,0],
+		[0,0,1,0],
+		[1,1,0,2],
+		[3,2,1,2],
+		[0,2,3,3]
+	],
+	values: [2]
+}
+
+var tropical_island = {
+	name: 'Tropical Island',
+	categories: [
+		[3,0,1,0],
+		[0,1,1,2],
+		[1,1,1,0],
+		[2,3,1,2],
+		[0,3,2,2]
+	],
+	values: [1]
+}
+
+var springtime_picknic = {
+	name: 'Springtime Picknic',
+	categories: [
+		[1,0,1,1],
+		[1,0,0,1],
+		[0,3,0,0],
+		[3,0,0,3],
+		[3,3,0,0]
+	],
+	values: [1]
+}
+
+var exotic_exploration = {
+	name: 'Excotic Exploration',
+	categories: [
+		[0,0,0,2],
+		[2,3,3,3],
+		[0,2,0,1],
+		[0,3,1,2],
+		[2,3,2,0]
+	],
+	values: [3]
+}
+
+var mediterranean_flavours = {
+	name: 'Mediterranean Flavours',
+	categories: [
+		[0,2,0,1],
+		[3,2,1,1],
+		[0,2,1,3],
+		[0,0,0,2],
+		[3,2,0,0]
+	],
+	values: [2]
+}
+
+var russian_roulette = {
+	name: 'Russian Roulette',
+	categories: [
+		[0,1,0,0],
+		[1,1,1,0],
+		[1,0,0,2],
+		[2,0,0,1],
+		[0,3,0,3]
+	],
+	values: [1]
+}
+
+var earth_and_salt = {
+	name: 'Earth and Salt of Mexico',
+	categories: [
+		[0,3,1,2],
+		[0,3,2,1],
+		[2,0,0,2],
+		[0,1,0,2],
+		[0,3,2,0]
+	],
+	values: [3]
+}
---
+++ logic.js
@@ -0,0 +1,96 @@
+// Change the three numbers below to adjust the weight for each type of score:
+
+// match on a flavor - the pack has the flavour you selected
+var mat_weight = 1;
+// relenvancy score - the flavour you selected is the highest ranked within its category for that kit, and more than 1
+var rel_weight = 2;
+// the weight of the intensity match 
+var val_weight = 2;
+
+function relevancy_weights(category) {
+	var max_value = Math.max.apply(Math, category);
+	return category.map(
+		function(tag) {
+			return ( tag > 1 && tag == max_value ) ? 1 * rel_weight : 0;
+		})
+}
+
+function match_weights(category) {
+	return category.map(
+		function(tag) {
+			return tag > 0 ? 1 * mat_weight : 0;
+		})
+}
+
+function score(weights, test) {
+	var sum = 0;
+	for (var i = 0; i < weights.length; i++) {
+		sum += weights[i] * test[i];
+	}
+	return sum;
+}
+
+function value_score(kit_value, test_value) {
+	//for value range 1-3
+	return test_value == 0 ? 0 : (2 - Math.abs(kit_value - test_value)) * val_weight;
+}
+
+var kits = [
+	fireplace_sipper, 
+	smoking_guns, 
+	berry_bunch, 
+	liquid_dessert, 
+	day_at_the_ocean, 
+	wild_wild_west, 
+	tropical_island, 
+	springtime_picknic, 
+	exotic_exploration, 
+	mediterranean_flavours, 
+	russian_roulette, 
+	earth_and_salt 
+];
+
+
+function get_result(test_input) {
+
+	var result_array = [];
+
+	for (var i = 0; i < kits.length; i++) {
+		var kit = kits[i];
+		var kit_rel_score = 0;
+		var kit_mat_score = 0;
+		var kit_val_score = 0;
+
+		for (var j = 0; j < kit.categories.length; j++) {
+			var category = kit.categories[j];
+
+			kit_rel_score += score(relevancy_weights(category), test_input.categories[j]);
+			kit_mat_score += score(match_weights(category), test_input.categories[j]);
+		};
+
+		for (var j = 0; j < kit.values.length; j++) {
+			kit_val_score += value_score(kit.values[j], test_input.values[j]);
+		};
+
+		result_array.push({
+			name: kit.name,
+			mat_score: kit_mat_score,
+			rel_score : kit_rel_score,
+			val_score: kit_val_score,
+			sum_score: kit_mat_score + kit_rel_score + kit_val_score
+		})
+
+
+	};
+
+	return result_array;
+
+}
+
+
+// The array.reduce version of score()
+// function score2(weights, test) {
+// 	return weights.reduce(function(sum, weight, i) {
+// 		return sum + weight * test[i];
+// 	}, 0);
+// }
